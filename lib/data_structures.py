1ebe5d19794c74bb599ef9ba54594fd399edf565
Parker Crosgrove <pdxparker3@gmail.com>
2023-03-14 15:38:22 -0600

    finished lab

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -17,22 +17,40 @@ spicy_foods = [
 ]
 
 def get_names(spicy_foods):
-    pass
-
+  
+   return [food["name"] for food in spicy_foods]
+  
+    
 def get_spiciest_foods(spicy_foods):
-    pass
-
+     return [food for food in spicy_foods if food["heat_level"] > 5]
+    
+    
 def print_spicy_foods(spicy_foods):
-    pass
+    pepper = "🌶"
+    spicy_food_list = [(spicy_foods[n]["name"], spicy_foods[n]["cuisine"], spicy_foods[n]["heat_level"]) for n in range(len(spicy_foods))]
+    individual_spicy_food = [spicy_food_list[n] for n in range(len(spicy_food_list))]
+    for n in range(len(individual_spicy_food)):
+        print(f"{individual_spicy_food[n][0]} ({individual_spicy_food[n][1]}) | Heat Level: {pepper * individual_spicy_food[n][2]}")
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
+    
+    [cuisine_food] = [spicy_foods[n] for n in range(len(spicy_foods)) if spicy_foods[n]["cuisine"] == cuisine]
+    return cuisine_food
 
 def print_spiciest_foods(spicy_foods):
-    pass
-
+     pepper = "🌶"
+     spiciest_food_list = [(spicy_foods[n]["name"], spicy_foods[n]["cuisine"], spicy_foods[n]["heat_level"]) for n in range(len(spicy_foods)) if spicy_foods[n]["heat_level"] > 5]
+     ind_spiciest_foods = [spiciest_food_list[n] for n in range(len(spiciest_food_list))]
+     for n in range(len(ind_spiciest_foods)):
+        print(f"{ind_spiciest_foods[n][0]} ({ind_spiciest_foods[n][1]}) | Heat Level: {pepper * ind_spiciest_foods[n][2]}")
+    
 def get_average_heat_level(spicy_foods):
+    spicy_food_heats = [spicy_foods[n]["heat_level"] for n in range(len(spicy_foods))]
+    avg_heat = sum([spicy_food_heats[n] for n in range(len(spicy_food_heats))]) / len(spicy_food_heats)
+    return avg_heat
     pass
 
 def create_spicy_food(spicy_foods, spicy_food):
+    spicy_foods.append(spicy_food)
+    return spicy_foods
     pass
